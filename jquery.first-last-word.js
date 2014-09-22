19929b701cfcfca4f186a99d6fb018f9d35c8747
Dwayne Charrington <dwaynecharrington@gmail.com>
2014-09-22 18:20:09 +1000

    Replacing the first and last word with span tags

--- jquery.first-last-word.js
+++ jquery.first-last-word.js
@@ -1,26 +1,30 @@
 ;(function($, undefined) {
 
 	$.fn.lastWord = function() {
-		this.html(this.text().replace(/(\S+)\s*$/, '<span class="last-word">$1</span>'));
+	  var text = this.html().trim().split(" ");
+	  var last = text.pop();
+	  this.html(text.join(" ") + (text.length > 0 ? " <span class='last-word'>" + last + "</span>" : last));
 	};
 
 
 	$.fn.firstWord = function() {
-		this.html(this.text().replace(/^(\w+)/, '<span class="first-word">$1</span>'));		
+	  var text = this.html().trim().split(" ");
+	  var first = text.shift();
+	  this.html((text.length > 0 ? "<span class='first-word'>"+ first + "</span> " : first) + text.join(" "));
 	};
 
 	jQuery.extend(jQuery.expr[':'], { 
 
 		firstWord: function(elem, index, match) {
 			var $elem = $(elem);
-			var text  = $elem.text();
+			var text  = $elem.html();
 
 			return this.html(text.replace(/^(\w+)/, '<span class="first-word">$1</span>'));
 		},
 
 		lastWord: function(elem, index, match) {
 			var $elem = $(elem);
-			var text  = $elem.text();
+			var text  = $elem.grml();
 
 			return this.html(text.replace(/(\S+)\s*$/, '<span class="last-word">$1</span>'));
 		}
