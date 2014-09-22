724b5de5f73c3be12202c441ec35392371f7f5a0
Dwayne Charrington <dwaynecharrington@gmail.com>
2014-09-22 18:22:23 +1000

    Spacing for the plugin

--- jquery.first-last-word.js
+++ jquery.first-last-word.js
@@ -1,30 +1,32 @@
 ;(function($, undefined) {
 
 	$.fn.lastWord = function() {
-	  var text = this.html().trim().split(" ");
-	  var last = text.pop();
-	  this.html(text.join(" ") + (text.length > 0 ? " <span class='last-word'>" + last + "</span>" : last));
+		var text = this.html().trim().split(" ");
+		var last = text.pop();
+
+		this.html(text.join(" ") + (text.length > 0 ? " <span class='last-word'>" + last + "</span>" : last));
 	};
 
 
 	$.fn.firstWord = function() {
-	  var text = this.html().trim().split(" ");
-	  var first = text.shift();
-	  this.html((text.length > 0 ? "<span class='first-word'>"+ first + "</span> " : first) + text.join(" "));
+		var text = this.html().trim().split(" ");
+		var first = text.shift();
+		
+		this.html((text.length > 0 ? "<span class='first-word'>"+ first + "</span> " : first) + text.join(" "));
 	};
 
 	jQuery.extend(jQuery.expr[':'], { 
 
 		firstWord: function(elem, index, match) {
 			var $elem = $(elem);
-			var text  = $elem.html();
+			var text  = $elem.text();
 
 			return this.html(text.replace(/^(\w+)/, '<span class="first-word">$1</span>'));
 		},
 
 		lastWord: function(elem, index, match) {
 			var $elem = $(elem);
-			var text  = $elem.grml();
+			var text  = $elem.text();
 
 			return this.html(text.replace(/(\S+)\s*$/, '<span class="last-word">$1</span>'));
 		}
